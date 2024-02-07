168dc9e9c6df8b7e3ad2f1385a24571ccd2f37f4
John Moore <jmoore@zedstar.org>
2024-02-07 12:57:59 +0000

    improve uri validation

--- lib/specification_util.ml
+++ lib/specification_util.ml
@@ -1,3 +1,18 @@
-let validate_target x = String.is_valid_utf_8 x
+
+let validate_uri_scheme uri =
+  Uri.scheme uri |> function
+  | Some scheme when scheme = "https" -> true
+  | _ -> false
+
+let validate_uri_host uri =
+  Uri.host uri |> function
+  | Some _ -> true
+  | None -> false
+
+let validate_uri x =
+  let uri = Uri.of_string x in
+  validate_uri_scheme uri && validate_uri_host uri
+
+let validate_target = validate_uri
 
 let validate_value x = String.is_valid_utf_8 x
