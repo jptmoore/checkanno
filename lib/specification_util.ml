f128ea7a52b864502442060642fed51e639e63a6
John Moore <jmoore@zedstar.org>
2024-02-07 13:56:54 +0000

    handle some optional fields

--- lib/specification_util.ml
+++ lib/specification_util.ml
@@ -13,6 +13,12 @@ let validate_uri x =
   let uri = Uri.of_string x in
   validate_uri_scheme uri && validate_uri_host uri
 
+let validate_id x =
+  match x with
+  | Some x -> validate_uri x
+  | None -> true
+  
 let validate_target = validate_uri
 
 let validate_value x = String.is_valid_utf_8 x
+
