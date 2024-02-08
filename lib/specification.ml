735cf2515505452adaad51756463fb62135fdd9c
John Moore <jmoore@zedstar.org>
2024-02-08 18:50:58 +0000

    redundant string format

--- lib/specification.ml
+++ lib/specification.ml
@@ -4,6 +4,5 @@ let validate ~data =
     let spec = Specification_j.specification_of_string data in
     Specification_v.validate_specification [] spec |> function
       | None -> Result.ok ()
-      | Some e -> Result.error (Printf.sprintf "%s"
-      (Atdgen_runtime.Util.Validation.string_of_error e))
+      | Some e -> Result.error (Atdgen_runtime.Util.Validation.string_of_error e)
   with e -> Result.error (Printexc.to_string e)
