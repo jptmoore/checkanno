05c9e7f64c2699faa9904e2fb7b012dc99fd7588
John Moore <jmoore@zedstar.org>
2024-02-08 17:27:14 +0000

    added missing exception handling

--- lib/specification.ml
+++ lib/specification.ml
@@ -1,7 +1,9 @@
 
 let validate ~data =
-  let spec = Specification_j.specification_of_string data in
-  Specification_v.validate_specification [] spec |> function
-    | None -> Result.ok "ok"
-    | Some e -> Result.error (Printf.sprintf "%s"
-    (Atdgen_runtime.Util.Validation.string_of_error e))
+  try
+    let spec = Specification_j.specification_of_string data in
+    Specification_v.validate_specification [] spec |> function
+      | None -> Result.ok "ok"
+      | Some e -> Result.error (Printf.sprintf "%s"
+      (Atdgen_runtime.Util.Validation.string_of_error e))
+  with e -> Result.error (Printexc.to_string e)
